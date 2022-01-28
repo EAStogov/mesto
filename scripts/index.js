de34aaa869627aec3d55a698e9a1611c1f5844a5
EgorStogov <egst94@yandex.ru>
2022-01-28 19:04:37 +0300

    feature: add some logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
 let edit = document.querySelector('.profile__edit-button');
+let add = document.querySelector('.profile__add-button');
 let popupClose = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
+let popups = document.querySelectorAll('.popup');
+let profileButtons = document.querySelector('.profile').querySelectorAll('button');
 let profileName = document.querySelector('.profile__name');
 let profileDesc = document.querySelector('.profile__description')
 let form = document.querySelector('.popup__form');
 let inputs = document.querySelectorAll('.popup__input');
 let inputName;
 let inputDesc;
+
+let buttonOnPage = document.querySelectorAll('.page__button');
+let popupActive = popups[1];
 
 inputs.forEach(element => {
   if (element.getAttribute('name') === 'name') {
@@ -16,34 +21,57 @@ inputs.forEach(element => {
   }
 })
 
-function openPopup() {
-  popup.classList.add('popup_opened');
+function findPopupById(id) {
+  let findedPopup;
+  popups.forEach(popup => {
+    if (popup.getAttribute('id') === id) {
+      findedPopup = popup;
+      return findedPopup;
+    }
+  })
+  return findedPopup;
+}
+
+function openPopup(id) {
+  popupActive = findPopupById(id);
+  popupActive.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputDesc.value = profileDesc.textContent;
+  console.log(popupActive);
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function closePopup(id) {
+  popupActive.classList.remove('popup_opened');
 }
 
-function submitChanges(event) {
-  event.preventDefault();
-  profileName.textContent = inputName.value;
-  profileDesc.textContent = inputDesc.value;
-  closePopup();
+function submitChanges(evt) {
+  evt.preventDefault();
+  if (evt.target.getAttribute('id') === 'edit') {
+    profileName.textContent = inputName.value;
+    profileDesc.textContent = inputDesc.value;
+  } else {
+    
+  }
+  
+  closePopup(evt.target.getAttribute('id'));
 }
 
-edit.addEventListener('click', openPopup);
+profileButtons.forEach(button => {
+  console.log(button);
+  button.addEventListener('click', (evt) => {openPopup(evt.target.getAttribute('id'))});
+});
 
-popupClose.addEventListener('click', closePopup);
+popups.forEach(popup => {
+  popup.querySelector('.popup__close').addEventListener('click', closePopup);
+})
 
 form.addEventListener('submit', submitChanges);
 
-popup.addEventListener('mousedown', function(event) {
-  if (event.path[0] === document.querySelector('.popup')) {
-    closePopup();
-  }
-});
+// popups.addEventListener('mousedown', function(event) {
+//   if (event.path[0] === popupActive) {
+//     closePopup();
+//   }
+// });
 
 document.addEventListener('keydown', function(event) {
   if (event.code === 'Escape') {
