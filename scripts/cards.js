eb1ae72add71f68d589b716287c67bfc79b74d2c
EgorStogov <egst94@yandex.ru>
2022-01-30 11:19:58 +0300

    refactor: replace initialCards to the new js

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Россия',
+    link: './images/Russia.jpg'
+  },
+  {
+    name: 'Хорватия',
+    link: './images/Croatia.jpg'
+  },
+  {
+    name: 'Германия',
+    link: './images/Germany.jpg'
+  },
+  {
+    name: 'Турция',
+    link: './images/Turkey.jpg'
+  },
+  {
+    name: 'Вьетнам',
+    link: './images/Vietnam.jpg'
+  },
+  {
+    name: 'Япония',
+    link: './images/Japan.jpg'
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -11,32 +11,7 @@ let inputLink;
 const popupImage = document.querySelector('.popup__image');
 const popupDesc = document.querySelector('#image-description');
 
-const initialCards = [
-  {
-    name: 'Россия',
-    link: './images/Russia.jpg'
-  },
-  {
-    name: 'Хорватия',
-    link: './images/Croatia.jpg'
-  },
-  {
-    name: 'Германия',
-    link: './images/Germany.jpg'
-  },
-  {
-    name: 'Турция',
-    link: './images/Turkey.jpg'
-  },
-  {
-    name: 'Вьетнам',
-    link: './images/Vietnam.jpg'
-  },
-  {
-    name: 'Япония',
-    link: './images/Japan.jpg'
-  }
-];
+
 
 function addCard(place, link, isAppend) {
   const templateCard = document.querySelector('#card-template').content;
