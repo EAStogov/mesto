e9a17682e4162d51e7dfcac5f8eeea15c013854f
Egor Stogov <egstegst@yandex.ru>
2022-02-10 13:03:34 +0300

    feature: add validation forms

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,11 +1,13 @@
-function setEventListeners(form, inputSelector, submitButton) {
+function setEventListeners(form, inputSelector, submitButton, inactiveButtonClass) {
   const inputs = Array.from(form.querySelectorAll(inputSelector));
   const formButton = form.querySelector(submitButton);
 
+  toggleButtonState(inputs, formButton, inactiveButtonClass);
+
   inputs.forEach(input => {
     input.addEventListener('input', () => {
-      checkInputValidity(form, input)
-      toggleButtonState(inputs, formButton);
+      checkInputValidity(form, input);
+      toggleButtonState(inputs, formButton, inactiveButtonClass);
     })
   });
 }
@@ -19,14 +21,14 @@ function checkInputValidity(form, input, inputErrorClass, errorClass) {
 }
 
 function showInputError(form, input, errorMessage, inputErrorClass, errorClass) {
-  const errorElement = form.querySelector(`.popup__input-error_type_${input.id}`);
+  const errorElement = form.querySelector(`.popup__input-error_type_${input.name}`);
   input.classList.add(inputErrorClass);
   errorElement.textContent = errorMessage;
   errorElement.classList.add(errorClass);
 }
 
 function removeInputError(form, input, inputErrorClass, errorClass) {
-  const errorElement = form.querySelector(`.popup__input-error_type_${input.id}`);
+  const errorElement = form.querySelector(`.popup__input-error_type_${input.name}`);
   input.classList.remove(inputErrorClass);
   errorElement.textContent = '';
   errorElement.classList.remove(errorClass);
@@ -35,8 +37,10 @@ function removeInputError(form, input, inputErrorClass, errorClass) {
 function toggleButtonState(inputs, formButton, inactiveButtonClass) {
   if (containsInvalidInput(inputs)) {
     formButton.classList.add(inactiveButtonClass);
+    formButton.setAttribute('disabled', 'disabled');
   } else {
     formButton.classList.remove(inactiveButtonClass);
+    formButton.removeAttribute('disabled');
   }
 }
 
@@ -49,7 +53,7 @@ function containsInvalidInput(inputs) {
 function enableValidation(obj) {
   const forms = Array.from(document.querySelectorAll(obj.formSelector));
   forms.forEach(form => {
-    setEventListeners(form, obj.inputSelector, obj.submitButtonSelector);
+    setEventListeners(form, obj.inputSelector, obj.submitButtonSelector, obj.inactiveButtonClass);
     form.addEventListener('submit', (evt) => {
       evt.preventDefault();
     });
