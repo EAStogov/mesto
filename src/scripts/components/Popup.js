ef4dd38925bae2aa20f2f28c4e0a45f717b0d5f9
EgorStogov <egst94@yandex.ru>
2022-03-22 16:37:45 +0300

    refactor: change method name & make removing listeners

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -1,35 +1,41 @@
 export default class Popup {
   constructor(selector) {
     this.popup = document.querySelector(selector);
+    this._handleEscClose = this._handleEscClose.bind(this);
+    this._handlePopupClose = this._handlePopupClose.bind(this);
   }
 
   open() {
     this.popup.classList.add('popup_opened');
+    this.setEventListeners();
   };
 
   close() {
     this.popup.classList.remove('popup_opened');
+    this.removeEventListeners();
   };
 
   _handleEscClose(evt) {
     if (evt.key === 'Escape') {
       this.close();
+      console.log('escape')
     }
   };
 
-  _overlayAndButtonClose(evt) {
+  _handlePopupClose(evt) {
     if ((evt.target === this.popup) || evt.target.classList.contains('popup__close')) {
       this.close();
+      console.log('хуяк');
     }
   }
 
   setEventListeners() {
-    this.popup.addEventListener('mousedown', this._overlayAndButtonClose.bind(this));
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    this.popup.addEventListener('mousedown', this._handlePopupClose);
+    document.addEventListener('keydown', this._handleEscClose);
   };
 
-  // removeEventListeners() {
-  //   this.popup.removeEventListener('mousedown', this._overlayAndButtonClose.bind(this));
-  //   document.removeEventListener('keydown', this._handleEscClose.bind(this));
-  // }
+  removeEventListeners() {
+    this.popup.removeEventListener('mousedown', this._handlePopupClose);
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
 }
