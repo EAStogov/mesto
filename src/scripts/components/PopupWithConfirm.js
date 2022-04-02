732d21401a1448525e4302ae79781cd4224901b3
EgorStogov <egst94@yandex.ru>
2022-04-02 14:35:59 +0300

    feature: add deleting status to the button's text

--- src/scripts/components/PopupWithConfirm.js
+++ src/scripts/components/PopupWithConfirm.js
@@ -3,32 +3,35 @@ import Popup from "./Popup.js";
 export default class PopupWithConfirm extends Popup {
   constructor(selector, buttonSelector) {
     super(selector);
-    this.button = this._popup.querySelector(buttonSelector);
+    this._submitButton = this._popup.querySelector(buttonSelector);
+    this._buttonText = this._submitButton.textContent;
     this._deleteItem = this._deleteItem.bind(this);
   }
 
   setEventListeners() {
     super.setEventListeners();
-    this.button.addEventListener('click', this._deleteItem);
+    this._submitButton.addEventListener('click', this._deleteItem);
   }
 
   removeEventListeners() {
     super.removeEventListeners();
-    this.button.removeEventListener('click', this._deleteItem);
+    this._submitButton.removeEventListener('click', this._deleteItem);
   }
 
   open(item, api, cardId) {
     super.open();
-    console.log(item);
+    this._submitButton.textContent = this._buttonText;
     this._api = api;
     this._cardId = cardId;
     this._item = item;
   }
 
-  _deleteItem() {
+  _deleteItem(evt) {
+    evt.preventDefault();
+    this._submitButton.textContent = 'Удаление...';
     this._api.deleteCard(this._cardId).then(() => {
-    this.close();
-    this._item.remove();
+      this.close();
+      this._item.remove();
     })
   }
 }
